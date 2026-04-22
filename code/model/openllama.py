56b98864652818584ba56b514956b6bd2979ee29
Charlie-Claw <charlie-claw@users.noreply.github.com>
2026-04-22 08:04:45 +0000

    feat: wire MultiScaleAnomalyFusion + add anomaly_fusion trainable + fix max_tgt_len to 1024

--- code/model/openllama.py
+++ code/model/openllama.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 from .ImageBind import *
 from .ImageBind import data
 from .modeling_llama import LlamaForCausalLM
-from .AnomalyGPT_models import LinearLayer, PromptLearner, TextPromptAdapter
+from .AnomalyGPT_models import LinearLayer, PromptLearner, TextPromptAdapter, MultiScaleAnomalyFusion
 from transformers import StoppingCriteria, StoppingCriteriaList, LlamaTokenizer, LlamaForCausalLM
 from utils.loss import FocalLoss, BinaryDiceLoss
 from .prompts import get_prompts
@@ -204,6 +204,7 @@ class OpenLLAMAPEFTModel(nn.Module):
         self.prompt_learner = PromptLearner(1, 4096)
 
         self.text_adapter = TextPromptAdapter(embed_dim=1024, hidden_dim=128)
+        self.anomaly_fusion = MultiScaleAnomalyFusion(n_layers=4)
 
         self.loss_focal = FocalLoss()
         self.loss_dice = BinaryDiceLoss()
@@ -494,7 +495,7 @@ class OpenLLAMAPEFTModel(nn.Module):
             for num in range(len(anomaly_maps)):
                 anomaly_maps[num] = anomaly_maps[num][:,1,:,:]
 
-            anomaly_map_all = torch.mean(torch.stack(anomaly_maps, dim=0), dim=0).unsqueeze(1)
+            anomaly_map_all = self.anomaly_fusion(anomaly_maps)
 
             if random.randint(0,1) == 0 and len(inputs['img_paths']) == len(image_paths):
 
@@ -581,7 +582,7 @@ class OpenLLAMAPEFTModel(nn.Module):
             for num in range(len(anomaly_maps)):
                 anomaly_maps[num] = anomaly_maps[num][:,1,:,:]
 
-            anomaly_map_all = torch.mean(torch.stack(anomaly_maps, dim=0), dim=0).unsqueeze(1)
+            anomaly_map_all = self.anomaly_fusion(anomaly_maps)
 
             anomaly_map_prompts = self.prompt_learner(anomaly_map_all)
 
@@ -639,7 +640,7 @@ class OpenLLAMAPEFTModel(nn.Module):
                 anomaly_map = torch.softmax(anomaly_map, dim=1)
                 anomaly_maps.append(anomaly_map[:,1,:,:])
 
-            anomaly_map_ret = torch.mean(torch.stack(anomaly_maps, dim=0), dim=0).unsqueeze(1)
+            anomaly_map_ret = self.anomaly_fusion(anomaly_maps)
             # anomaly_map_all = anomaly_map_ret.unsqueeze(1).repeat((1,3,1,1))
             # anomaly_map_feature, _, _ = self.encode_image_from_tensor(anomaly_map_all)
             # image_embeds = anomaly_map_feature + image_embeds
--- code/scripts/train_mvtec.sh
+++ code/scripts/train_mvtec.sh
@@ -7,7 +7,7 @@ deepspeed --include localhost:0,1 --master_port 28400 train_mvtec.py \
     --imagebind_ckpt_path ../pretrained_ckpt/imagebind_ckpt/imagebind_huge.pth \
     --vicuna_ckpt_path ../pretrained_ckpt/vicuna_ckpt/7b_v0/ \
     --delta_ckpt_path ../pretrained_ckpt/pandagpt_ckpt/7b/pytorch_model.pt \
-    --max_tgt_len 512 \
+    --max_tgt_len 1024 \
     --data_path ../data/pandagpt4_visual_instruction_data.json \
     --image_root_path /workspace/Master-Thesis/data/images/ \
     --save_path ./ckpt/train_mvtec/ \
